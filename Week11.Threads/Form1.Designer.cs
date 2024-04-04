4cff191175f03224828bb55c33cb248de1297cc8
vylnp <vylnp@V>
2024-04-04 10:48:40 -0400

    Add project files.

---
+++ Week11.Threads/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Week11.Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(32, 41);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(209, 26);
+            this.textBoxSearch.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(261, 31);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(173, 47);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 20;
+            this.listBoxResult.Location = new System.Drawing.Point(32, 100);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(402, 424);
+            this.listBoxResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 546);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.textBoxSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ListBox listBoxResult;
+    }
+}
+
---
+++ Week11.Threads/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week11.Threads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Clear previous results
+            listBoxResult.Items.Clear();
+            // Validate input
+            if (!int.TryParse(textBoxSearch.Text, out int input) || input <= 0)
+            {
+                MessageBox.Show("Please enter a valid positive integer.");
+                return;
+            }
+
+            // Start generating Fibonacci numbers in a background thread
+            Task search = Task.Run(() => SearchFibonacci(input));
+            // Wait for all tasks to complete
+            await Task.WhenAll(search);
+        }
+        private void SearchFibonacci(int input)
+        {
+            for (int i = 0; i < input; i++)
+            {
+                // Calculate Fibonacci number
+                int fib = Fibonacci(i);
+
+                // Update UI with the generated Fibonacci number
+                BeginInvoke((Action)(() =>
+                {
+                    listBoxResult.Items.Add(fib);
+                }));
+            }
+        }
+
+        // Method to calculate Fibonacci numbers
+        private int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
